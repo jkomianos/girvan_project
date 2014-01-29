7528e575491981d7ac955e5b1b3d3a5d0b4e1c3f
jkomianos <jkomianos@gmail.com>
2014-01-29 15:48:13 -0500

    Added attractor count code

--- genenetwork.py
+++ genenetwork.py
@@ -7,20 +7,24 @@ import math
 import time
 import sys
 import copy
+import itertools
+import string
+
 
 """""""""""""""""""""""""""""""""""""""""""""""
 A geneNetwork class for gene network analysis.
 
 James Komianos
-Last Edited: 1/13/14
+Last Edited: 1/29/14
 """""""""""""""""""""""""""""""""""""""""""""""
 
 class geneNetwork:
 
-	G = nx.Graph()
+	G = None
 	GPerm = None
 
 	#To keep track of node values for updating
+	#Hold[value, Frozen(T/F)]
 	GNodes = {}
 	GPermNodes = {}
 
@@ -36,13 +40,15 @@ class geneNetwork:
 	def __init__(self, preset="RANDOM", numNodes=1000, 
 				connectionsPerNode=10, nodeTuples=None, edgeTuples=None):
 
+		self.G = nx.Graph()
+
 		if preset == "RANDOM":
 			#Add nodes
 			for i in xrange(0, numNodes):
 
 				state = 1 if random.random() >= 0.5 else 0
 				self.G.add_node(i, value=state)
-				self.GNodes[i] = state
+				self.GNodes[i] = [state, False]
 
 			#Now add edges
 			for i in xrange(0, numNodes):
@@ -62,10 +68,65 @@ class geneNetwork:
 		if preset == "MANUAL":
 			for nodeTuple in nodeTuples:
 				self.G.add_node(nodeTuple[0], value=nodeTuple[1])
-				self.GNodes[nodeTuple[0]] = nodeTuple[1]
+				self.GNodes[nodeTuple[0]] = [nodeTuple[1], False]
 
 			self.G.add_weighted_edges_from(edgeTuples)
 			self.numGenes = len(nodeTuples)
+
+
+	"""
+	Finds frozen nodes in graph, removes them from dict
+	"""
+	def findFrozenNodes(self, threshold):
+
+		for n in self.G:
+
+			weights = []	
+			for neighbor in self.G.neighbors_iter(n):
+				weights.append(self.G[n][neighbor]['weight'])
+
+			numEdges = len(weights)	
+			frozen = True
+			truthTable = list(itertools.product([0,1], repeat = numEdges))
+
+			#Determine if node is frozen
+			for values in truthTable:
+				sumN = 0
+				for i in xrange(0, numEdges):
+					sumN += values[i] * weights[i]
+
+				if (sumN > threshold):
+					frozen = False
+					break
+
+			#If in fact frozen, remove from GNodes
+			if frozen: self.GNodes[n][1] = True	
+
+		#Remove from permutation if it exists	
+		if self.GPerm != None:
+
+			for n in self.GPerm:
+
+				weights = []	
+				for neighbor in self.GPerm.neighbors_iter(n):
+					weights.append(self.GPerm[n][neighbor]['weight'])
+
+				numEdges = len(weights)	
+				frozen = True
+				truthTable = list(itertools.product([0,1], repeat = numEdges))
+
+				#Determine if node is frozen
+				for values in truthTable:
+					sumN = 0
+					for i in xrange(0, numEdges):
+						sumN += values[i] * weights[i]
+
+					if (sumN > threshold):
+						frozen = False
+						break
+
+				#If in fact frozen, remove from GNodes
+				if frozen: self.GPermNodes[n][1] = True		
 
 
 	"""
@@ -82,7 +143,7 @@ class geneNetwork:
 			for i in xrange(0, self.numGenes):
 				state = 1 if random.random() >= 0.5 else 0
 				self.G.add_node(i, value=state)
-				self.GNodes[i] = state
+				self.GNodes[i][0] = state
 
 			#Now add edges
 			for i in xrange(0, self.numGenes):
@@ -96,61 +157,56 @@ class geneNetwork:
 		else:
 			for n in self.G:
 				state = 1 if random.random() >= 0.5 else 0
-				self.G.node[n]['value']	= self.GNodes[n] = state
+				self.G.node[n]['value']	= self.GNodes[n][0] = state	
 
 	"""
 	Update graphs using thresholding
 	"""	
 	def update(self, threshold):
 
-		GNodesChanged = []
-		GPermNodesChanged = []
-
 		#Iterate over all nodes
-		for n in self.G:
-			sumN = 0
-			#Find all adjacents
-			for neighbor in self.G.neighbors_iter(n):
-				#Get edge weight
-				value = self.GNodes[neighbor]
-				weight = self.G[n][neighbor]['weight']
-				sumN += value * weight	
+		for n in self.GNodes.keys():
 
-			#Update node based on sign of sum
-			if (sumN > threshold): 
-				self.G.node[n]['value'] = 1
-				if self.GNodes[n] != 1: GNodesChanged.append(n)
-			else: 
-				self.G.node[n]['value'] = 0
-				if self.GNodes[n] != 0: GNodesChanged.append(n)
+			if (self.GNodes[n][1] != True):
+				sumN = 0
+				#Find all adjacents
+				for neighbor in self.G.neighbors_iter(n):
+					#Get edge weight
+					value = self.GNodes[neighbor][0]
+					weight = self.G[n][neighbor]['weight']
+					sumN += value * weight	
+
+				#Update node based on sign of sum
+				valueN = self.GNodes[n][0]
+				if (sumN > threshold): 
+					self.G.node[n]['value'] = 1
+					if valueN != 1: self.GNodes[n][0] = 1
+				else: 
+					self.G.node[n]['value'] = 0
+					if valueN != 0: self.GNodes[n][0] = 0
 
 		#Update the permutation if it exists
 		if self.GPerm != None:	
-			for n in self.GPerm:
-				sumN = 0
-				#Find all adjacents
-				for neighbor in self.GPerm.neighbors_iter(n):
-					#Get edge weight 
-					value = self.GPermNodes[neighbor]
-					weight = self.GPerm[n][neighbor]['weight']
-					sumN += value * weight
+			for n in self.GPermNodes.keys():
 
-				#Update node based on sign of sum	
-				if (sumN > threshold): 
-					self.GPerm.node[n]['value'] = 1 
-					if self.GPermNodes[n] != 1: GNodesChanged.append(n)
-				else: 
-					self.GPerm.node[n]['value'] = 0
-					if self.GPermNodes[n] != 0: GPermNodesChanged.append(n)
+				if (self.GPermNodes[n][1] != True):
 
-		#Update the changed nodes in the dictionary
-		for n in GNodesChanged:
-			currentVal = self.GNodes[n]
-			self.GNodes[n] = 1 if currentVal == 0 else 0	
+					sumN = 0
+					#Find all adjacents
+					for neighbor in self.GPerm.neighbors_iter(n):
+						#Get edge weight 
+						value = self.GPermNodes[neighbor][0]
+						weight = self.GPerm[n][neighbor]['weight']
+						sumN += value * weight
 
-		for n in GPermNodesChanged:
-			currentVal = self.GPermNodes[n]
-			self.GPermNodes[n] = 1 if currentVal == 0 else 0
+					#Update node based on sign of sum	
+					valuePermN = self.GPermNodes[n][0]
+					if (sumN > threshold): 
+						self.GPerm.node[n]['value'] = 1 
+						if valuePermN != 1: self.GPermNodes[n][0] = 1
+					else: 
+						self.GPerm.node[n]['value'] = 0
+						if valuePermN != 0: self.GPermNodes[n][0] = 0
 					
 	"""
 	Create permutation of current graph
@@ -176,11 +232,11 @@ class geneNetwork:
 
 		if other == None:
 			for i in xrange(0, self.numGenes):
-				if self.G.node[i]['value'] != self.GPerm.node[i]['value']:
+				if self.GNodes[i][0] != self.GPermNodes[i][0]:
 					hammingDist += 1
 		else:
 			for i in xrange(0, self.numGenes):
-				if self.G.node[i]['value'] != other.node[i]['value']:
+				if self.GNodes[i][0] != other.node[i]['value']:
 					hammingDist += 1
 		
 		return hammingDist
@@ -188,7 +244,9 @@ class geneNetwork:
 	"""
 	Compute the saturated hamming distance between G and GPerm
 	"""
-	def calculateSatHammingDist(self, numTimeSteps=10, threshold=0):	
+	def calculateSatHammingDist(self, numTimeSteps, threshold):	
+
+		self.findFrozenNodes(threshold=threshold)
 
 		for t in xrange(0, numTimeSteps):
 			#Update, calculate hamming distance
@@ -200,9 +258,9 @@ class geneNetwork:
 	Compute a set of data representing avg saturated hamming 
 	dist vs thresholds, plot it
 	"""
-	def generateHammingDistVsThreshold(self, thresholdMin=0, thresholdMax=10,
-										thresholdStep=1, numICsPerThreshold=10,
-										numTimeStepsToSaturate=10, diffPermutation = 0.01):
+	def generateHammingDistVsThreshold(self, thresholdMin, thresholdMax,
+										thresholdStep, numICsPerThreshold,
+										numTimeStepsToSaturate, diffPermutation):
 	
 		self.createPermutation(diffThreshold=diffPermutation)
 
@@ -214,7 +272,7 @@ class geneNetwork:
 			for i in xrange(0, numTimeStepsToSaturate):
 				#Create permutation, calculate hammingDist after 
 				hammingDistsForT.append(self.calculateSatHammingDist(numTimeSteps=numTimeStepsToSaturate, threshold=t))	
-	    		self.reset(resetEdges=True)	
+	    		self.reset(resetEdges=False)	
 	    		self.createPermutation(diffThreshold=diffPermutation)
 
 			avgHammingDists.append(sum(hammingDistsForT) / float(len(hammingDistsForT)))
@@ -239,15 +297,18 @@ class geneNetwork:
 
 		X = [0] * self.numGenes #gene expression factor
 
+		self.findFrozenNodes(threshold=threshold)
+
 		for t in xrange(0, stop):
 			#Do not add to expression factor
-			if t < start:
+			if t < start: 
 				self.update(threshold=threshold)
+				
 			#Add to expression factor	
 			else:
 				self.update(threshold=threshold)
 				for n, value in self.GNodes.iteritems():
-					X[n] += value
+					X[n] += value[0]
 
 		#Now, normalize
 		geneExpFactor = [float(value) / (stop - start) for value in X]
@@ -256,15 +317,13 @@ class geneNetwork:
 	"""
 	Calculate expression diversity for this network	
 	"""
-	def expressionDiversity(self, threshold=0, numICs=100):
+	def expressionDiversity(self, threshold, numICs):
 	
 		allX = []
 		expDiv = 0
 		#Create expression factors
 		for i in xrange(0, numICs):
-			t1Exp = time.time()
 			allX.append(self.createGeneExpFactor(threshold=threshold))
-			t2Exp = time.time()
 			self.reset(resetEdges=False)
 
 		#Now, sum over all expression factors
@@ -284,8 +343,8 @@ class geneNetwork:
 	Compute a set of data representing gene expression 
 	diversity vs thresholds, plot it
 	"""
-	def generateExpDivVsThreshold(self, thresholdMin=0, thresholdMax=10,
-										thresholdStep=1, numICsPerThreshold=10):
+	def generateExpDivVsThreshold(self, thresholdMin, thresholdMax,
+										thresholdStep, numICsPerThreshold):
 	
 		thetas = []
 		expressionDiversities = []
@@ -295,7 +354,7 @@ class geneNetwork:
 			thetas.append(t)
 			expressionDiversities.append(self.expressionDiversity(
 								threshold=t, numICs=numICsPerThreshold))	
-			self.reset(resetEdges=True)
+			self.reset(resetEdges=False)
 
 	    #Now, plot this using matplotlib
 		plt.clf()
@@ -304,6 +363,94 @@ class geneNetwork:
 		plt.ylabel('Gene Expression Diversity')	
 		plt.grid(True)
 		plt.savefig("geneExpressionDiversity.jpg")
+
+
+	"""
+	Produces a bitstring of the current state
+	"""
+	def getStateBitstring(self):	
+		return ''.join([str(val[0]) for val in self.GNodes.values()])
+
+	"""
+	Given an initial condition, find a periodic orbit and save it
+	"""
+	def findPeriodicOrbit(self, threshold):
+
+		#Hash table to store the orbit
+		orbitTable = {}
+		#Find frozen nodes
+		self.findFrozenNodes(threshold = threshold)
+
+		orbitFound = False
+		start = True
+		currentState = ""
+		nextState = ""
+		while not orbitFound:
+
+			currentState = self.getStateBitstring() if start else nextState
+			self.update(threshold = threshold)
+			nextState = self.getStateBitstring()
+
+			#Check if nextState is in orbit
+			if nextState in orbitTable:
+				orbitFound = True
+			else:
+				orbitTable[currentState] = nextState
+			start = False
+
+		#We have found the orbit, now save it (starting at nextState)
+		finalOrbit = []
+		saveOrbit = True
+		orbitState = firstOrbitState = nextState
+
+		#Save the orbit
+		while saveOrbit:
+			finalOrbit.append(orbitState)
+			try:
+				orbitState = orbitTable[orbitState]
+				if orbitState == firstOrbitState: saveOrbit = False
+			except KeyError:
+				saveOrbit = False
+
+		return finalOrbit
+
+	"""
+	Counting attractors for a given number of ICs
+	"""
+	def countAttractors(self, numICs, threshold):
+
+		attractors = []
+		for i in xrange(0, numICs):
+			orbit = self.findPeriodicOrbit(threshold=threshold)
+			if orbit not in attractors:
+				attractors.append(orbit)
+			self.reset(resetEdges=False)
+
+		return len(attractors)
+
+
+	"""
+	Create attractor count vs threshold plot
+	"""
+	def generateAttractorsVsThreshold(self, thresholdMin, thresholdMax,
+										thresholdStep, numICsPerThreshold):
+
+		thetas = []
+		attractors = []
+
+		for t in np.arange(thresholdMin, thresholdMax, thresholdStep):
+
+			thetas.append(t)
+			attractors.append(self.countAttractors(numICs=numICsPerThreshold, threshold=t))	
+			self.reset(resetEdges=False)
+
+	    #Now, plot this using matplotlib
+		plt.clf()
+		plt.plot(thetas, attractors)
+		plt.xlabel('Threshold')
+		plt.ylabel('Attractor Count')	
+		plt.grid(True)
+		plt.savefig("attractorCount.jpg")
 
 
 	"""
--- genetest.py
+++ genetest.py
@@ -5,40 +5,56 @@ import scipy as sp
 import numpy as np
 sys.path.append("~/work/girvan_project/")
 import genenetwork as gn
+import matplotlib.pyplot as plt
 
 """""""""""""""""""""""""""""""""""
 Code to test the geneNetwork class.
 
 James Komianos
-Last edited: 1/13/14
+Last edited: 1/29/14
 """""""""""""""""""""""""""""""""""
 
-def main():
-    
+def timingGEDTest():
+
     #Testing expression diversity code
     nStart = 200
-    nEnd = 1000
+    nEnd = 1600
 
     times = []
 
     for numGenes in xrange(nStart, nEnd, 100):
 
         network = gn.geneNetwork(preset="RANDOM", numNodes=numGenes, connectionsPerNode=int(0.01*numGenes))
-
         t1 = time.time()
         network.generateExpDivVsThreshold(thresholdMin=0, thresholdMax=1,
                                             thresholdStep=1, numICsPerThreshold=10)
         t2 = time.time()
-
         times.append(t2 - t1)
 
     #Plot the timings
     plt.clf()
-    plt.plot(times, xrange(nStart, nEnd, 100))
-    plt.xlabel('Time to compute')
-    plt.ylabel('Number of nodes') 
+    plt.plot(xrange(nStart, nEnd, 100), times)
+    plt.ylabel('Time to compute (s)')
+    plt.xlabel('Number of nodes') 
     plt.grid(True)
-    plt.savefig("GEDBenchmark.jpg")
+    plt.savefig("GEDBenchmark4.jpg")
+
+def periodicOrbitTest():
+
+    numGenes =25
+    network = gn.geneNetwork(preset="RANDOM", numNodes=numGenes, connectionsPerNode=int(0.2*numGenes))
+
+    #print network.findPeriodicOrbit(threshold = 0)
+    #print  network.countAttractors(numICs=100, threshold=0)
+
+    network.generateAttractorsVsThreshold(thresholdMin=-10, thresholdMax=15,
+                                        thresholdStep=0.5, numICsPerThreshold=300)
+
+
+def main():
+    
+    #timingGEDTest()
+    periodicOrbitTest()
 
 
 if __name__ == "__main__":
